73693fb99740cbbb0f941acf956076ac6d230eb3
Sarah Turek <snturek@gmail.com>
2015-09-09 09:48:23 -0600

    mongolabs

--- server/database.js
+++ server/database.js
@@ -12,4 +12,4 @@ var Kitty = new Schema(
 
 mongoose.model('kitties', Kitty);
 
-mongoose.connect('mongodb://heroku_jgp5xkjn:j65n1bqd4sdbju2tp16cpkvo7h@ds047682.  .com:47682/heroku_jgp5xkjn' || 'mongodb://localhost/crud-kitties');
+mongoose.connect('mongodb://heroku_jgp5xkjn:j65n1bqd4sdbju2tp16cpkvo7h@ds047682.mongolab.com:47682/heroku_jgp5xkjn' || 'mongodb://localhost/crud-kitties');
