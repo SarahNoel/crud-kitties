447cab4f4402fe8fdf3bcd3dfe8f1d422132e4fe
Sarah Turek <snturek@gmail.com>
2015-09-09 09:44:39 -0600

    dotenv

--- server/database.js
+++ server/database.js
@@ -12,4 +12,7 @@ var Kitty = new Schema(
 
 mongoose.model('kitties', Kitty);
 
-mongoose.connect('process.env.mongodb://heroku_jgp5xkjn:j65n1bqd4sdbju2tp16cpkvo7h@ds047682.  .com:47682/heroku_jgp5xkjn' || 'mongodb://localhost/crud-kitties');
+process.env.MONGOLAB_URI = 'mongodb://heroku_jgp5xkjn:j65n1bqd4sdbju2tp16cpkvo7h@ds047682.  .com:47682/heroku_jgp5xkjn';
+
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/crud-kitties');
